81b2a5a9ea0225a80ab6248f0eb90be3d6ad6fb7
Lauren West <laurenwest12@gmail.com>
2019-10-12 11:45:28 -0400

    initial folder setup

---
+++ src/Components/AllMentors.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+class AllMentors extends Component {
+  render() {
+    return <h1>Hello World</h1>;
+  }
+}
+
+export default AllMentors;
---
+++ src/Components/SingleMentor.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+class SingleMentor extends Component {
+  render() {
+    return <h1>Hello World</h1>;
+  }
+}
+
+export default SingleMentor;
