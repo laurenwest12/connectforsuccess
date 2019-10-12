f4879e7acfe182fac2c547da7e13bcdefb38803f
lgm527 <laurellmccaffrey@gmail.com>
2019-10-12 15:56:21 -0400

    filter set up

--- src/App.js
+++ src/App.js
@@ -7,19 +7,7 @@ import AllMentors from './Components/AllMentors';
 export default class App extends Component {
   state = {
     show: false,
-    mentor: {
-      firstName: 'Lauren',
-      lastName: 'West',
-      email: 'lwest@gmail.com',
-      location: 'Los Angeles',
-      industry: 'Data Analytics',
-      connection: ['ongoing'],
-      skills: ['interview prep', 'job search', 'networking'],
-      daysAvailable: ['Monday', 'Tuesday', 'Wednesday'],
-      timesAvailable: ['evening'],
-      bio:
-        'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
-    }
+    mentor: {}
   };
 
   showSingleMentor = mentor => {
@@ -27,7 +15,7 @@ export default class App extends Component {
   };
 
   showAll = () => {
-    this.setState({ show: false, mentor: [] });
+    this.setState({ show: false, mentor: {} });
   };
 
 
@@ -40,22 +28,14 @@ export default class App extends Component {
           alt="dress-for-success-logo"
           className="logo"
         ></img>
-        <div>
-          <h3>Location</h3>
-          <h3>Connection</h3>
-          <h3>Days</h3>
-          <h3>Times</h3>
-          <h3>Skills</h3>
-        </div>
         <button
           type="button"
           onClick={this.showAll}
-          className="allMentorsButton"
-        >
+          className="allMentorsButton">
           Mentors
         </button>
         {this.state.show ? (
-          <SingleMentor mentor={this.state.mentor} showAll={this.showAll} />
+          <SingleMentor mentor={this.state.mentor} />
         ) : (
           <AllMentors showSingleMentor={this.showSingleMentor} />
         )}
--- src/Components/AllMentors.js
+++ src/Components/AllMentors.js
@@ -5,7 +5,8 @@ import ava from '../ava.jpg';
 class AllMentors extends Component {
 
   state = {
-    mentors: []
+    mentors: [],
+    filters: []
   }
 
   componentDidMount(){
@@ -16,25 +17,62 @@ class AllMentors extends Component {
     return arr.join(' ')
   }
 
+  setFilters = () => {
+    console.log('hi');
+  }
+
+ filteredMentors = () => {
+  return this.state.mentors.map((p, i) => {
+  return <div className='mentor' key={i}>
+    <img src={ava} alt='mentor'/>
+    <h1 onClick={() => {this.props.showSingleMentor(p)}}>{p.firstName} {p.lastName}</h1>
+    <p>Location: {p.location}</p>
+    <p>Industry: {p.industry}</p>
+    <p>Skills: {this.arrayToList(p.skills)}</p>
+    <p>Connection: {this.arrayToList(p.connection)}</p>
+    <p>Days Available: {this.arrayToList(p.daysAvailable)}</p>
+    <p>Times Available: {this.arrayToList(p.timesAvailable)}</p>
+  </div>
+  })
+}
+
   render() {
 
-  const filteredMentors = this.state.mentors.map((p, i) => {
-    return <div className='mentor' key={i}>
-          <img src={ava} alt='mentor'/>
-          <h1>{p.firstName} {p.lastName}</h1>
-          <p>Location: {p.location}</p>
-          <p>Industry: {p.industry}</p>
-          <p>Skills: {this.arrayToList(p.skills)}</p>
-          <p>Connection: {this.arrayToList(p.connection)}</p>
-          <p>Days Available: {this.arrayToList(p.daysAvailable)}</p>
-          <p>Times Available: {this.arrayToList(p.timesAvailable)}</p>
-        </div>
-  })
 
     return (
-      <div className='mentors'>
-      {filteredMentors}
+      <>
+      <div id='filters'>
+        <h3>Location</h3>
+
+        <h3>Connection</h3>
+          <label>Casual<input type='checkbox' onChange={() => {this.setFilters('casual')}}/></label>
+          <label>Online<input type='checkbox' onChange={() => {this.setFilters('online')}}/></label>
+          <label>In-person<input type='checkbox' onChange={() => {this.setFilters('in-person')}}/></label>
+          <label>Ongoing<input type='checkbox' onChange={() => {this.setFilters('ongoing')}}/></label>
+        <h3>Days</h3>
+          <label>Monday<input type='checkbox' onChange={() => {this.setFilters('Monday')}}/></label>
+          <label>Tuesday<input type='checkbox' onChange={() => {this.setFilters('Tuesday')}}/></label>
+          <label>Wednesday<input type='checkbox' onChange={() => {this.setFilters('Wednesday')}}/></label>
+          <label>Thursday<input type='checkbox' onChange={() => {this.setFilters('Thursday')}}/></label>
+          <label>Friday<input type='checkbox' onChange={() => {this.setFilters('Friday')}}/></label>
+          <label>Saturday<input type='checkbox' onChange={() => {this.setFilters('Saturday')}}/></label>
+          <label>Sunday<input type='checkbox' onChange={() => {this.setFilters('Sunday')}}/></label>
+        <h3>Times</h3>
+          <label>Morning<input type='checkbox' onChange={() => {this.setFilters('morning')}}/></label>
+          <label>Afternoon<input type='checkbox' onChange={() => {this.setFilters('afternoon')}}/></label>
+          <label>Evening<input type='checkbox' onChange={() => {this.setFilters('evening')}}/></label>
+        <h3>Skills</h3>
+          <label>Interview Prep<input type='checkbox' onChange={() => {this.setFilters('interview prep')}}/></label>
+          <label>Salary Negotiation<input type='checkbox' onChange={() => {this.setFilters('salary negotiation')}}/></label>
+          <label>Job Search<input type='checkbox' onChange={() => {this.setFilters('job search')}}/></label>
+          <label>Networking<input type='checkbox' onChange={() => {this.setFilters('networking')}}/></label>
+          <label>Resume<input type='checkbox' onChange={() => {this.setFilters('resume')}}/></label>
+          <input type='submit' />
       </div>
+      <div className='mentors'>
+      {this.filteredMentors()}
+      </div>
+      </>
     );
   }
 }
