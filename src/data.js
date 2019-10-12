29d926dc3c2b726482f0dd29f59c523620076658
lgm527 <laurellmccaffrey@gmail.com>
2019-10-12 13:12:01 -0400

    data added

---
+++ src/data.js
@@ -0,0 +1,103 @@
+let data = [
+
+    {
+      firstName: 'Lauren',
+      lastName: 'West',
+      email: 'lwest@gmail.com',
+      location: 'Los Angeles',
+      industry: 'Data Analytics',
+      connection: ['ongoing'],
+      skills: ['interview prep', 'job search', 'networking'],
+      daysAvailable: ['Monday', 'Tuesday', 'Wednesday'],
+      timesAvailable: ['evening']
+    },
+    {
+      firstName: 'Liliana',
+      lastName: 'Delman',
+      email: 'lilianadelman@gmail.com',
+      location: 'Denver',
+      industry: 'Higher Education',
+      connection: ['casual', 'online'],
+      skills: ['interview prep', 'job search', 'resume'],
+      daysAvailable: ['Sunday', 'Saturday'],
+      timesAvailable: ['morning', 'afternoon']
+    },
+    {
+      firstName: 'Brooke',
+      lastName: 'Rothman',
+      email: 'brothman@gmail.com',
+      location: 'Paris',
+      industry: 'Design Strategy',
+      connection: ['online', 'ongoing'],
+      skills: ['interview prep', 'salary negotiation'],
+      daysAvailable: ['Sunday', 'Wednesday'],
+      timesAvailable: ['evening']
+    },
+    {
+      firstName: 'Coreen',
+      lastName: 'Rogers',
+      email: 'crogers@gmail.com',
+      location: 'New York',
+      industry: 'Higher Education',
+      connection: ['casual', 'online', 'in-person', 'ongoing'],
+      skills: ['interview prep', 'salary negotiation', 'job search', 'networking'],
+      daysAvailable: ['Monday', 'Tuesday', 'Wednesday'],
+      timesAvailable: ['evening']
+    },
+    {
+      firstName: 'Irina',
+      lastName: 'Bright',
+      email: 'ibright@gmail.com',
+      location: 'New York',
+      industry: 'Web Development',
+      connection: ['casual', 'online', 'in-person', 'ongoing'],
+      skills: ['interview prep', 'salary negotiation', 'job search', 'networking', 'resume'],
+      daysAvailable: ['Sunday', 'Monday', 'Tuesday', 'Wednesday'],
+      timesAvailable: ['morning', 'afternoon', 'evening']
+    },
+    {
+      firstName: 'Laurell',
+      lastName: 'McCaffrey',
+      email: 'lmccaffrey@gmail.com',
+      location: 'Berlin',
+      industry: 'Software Engineering',
+      connection: ['in-person', 'ongoing'],
+      skills: ['interview prep', 'salary negotiation', 'resume'],
+      daysAvailable: ['Sunday', 'Monday'],
+      timesAvailable: ['morning', 'afternoon']
+    },
+    {
+      firstName: 'Joan',
+      lastName: 'Smith',
+      email: 'jsmith@aol.com',
+      location: 'New York',
+      industry: 'Teacher',
+      connection: ['casual', 'online', 'in-person', 'ongoing'],
+      skills: ['interview prep', 'salary negotiation', 'job search', 'networking', 'resume'],
+      daysAvailable: ['Sunday', 'Monday', 'Tuesday', 'Wednesday'],
+      timesAvailable: ['morning', 'afternoon', 'evening']
+    },
+    {
+      firstName: 'Jane',
+      lastName: 'Doe',
+      email: 'jdoe@aol.com',
+      location: 'New York',
+      industry: 'Journalist',
+      connection: ['casual', 'online', 'in-person', 'ongoing'],
+      skills: ['interview prep', 'salary negotiation', 'job search', 'networking', 'resume'],
+      daysAvailable: ['Sunday', 'Monday', 'Tuesday', 'Wednesday'],
+      timesAvailable: ['morning', 'afternoon', 'evening']
+    },
+    {
+      firstName: 'Holly',
+      lastName: 'Brown',
+      email: 'hbrown@aol.com',
+      location: 'New York',
+      industry: 'Doctor',
+      connection: ['casual', 'online', 'ongoing'],
+      skills: ['networking', 'resume'],
+      daysAvailable: ['Sunday','Wednesday'],
+      timesAvailable: ['evening']
+    }
+
+]
