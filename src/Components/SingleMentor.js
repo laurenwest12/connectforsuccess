f7994ce162569a0bf7d05f7e26219f9365a1283a
Lauren West <laurenwest12@gmail.com>
2019-10-12 14:18:45 -0400

    single user complete

--- src/Components/SingleMentor.js
+++ src/Components/SingleMentor.js
@@ -2,7 +2,21 @@ import React, { Component } from 'react';
 
 class SingleMentor extends Component {
   render() {
-    return <h1>Hello World</h1>;
+    let { mentor } = this.props;
+    return (
+      <div className="singleMentorContainer">
+        <img
+          src="https://www.belr.com/wp-content/uploads/2017/06/avatar-placeholder-generic-1.jpg"
+          alt="mentor-image"
+          className="singleMentorImage"
+        ></img>
+        <div className="singleMentorName">
+          {mentor.firstName.toUpperCase()} {mentor.lastName.toUpperCase()}
+        </div>
+        <div className="singleMentorLocation">{mentor.location}</div>
+        <div className="singleMentorBio">{mentor.bio}</div>
+      </div>
+    );
   }
 }
 
